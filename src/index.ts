a67a01ef514f482e7d7acbbce3c666055f4099a9
User <belpc2012@gmail.com>
2022-06-23 23:28:48 +0300

    fix: add try catch to index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,28 +8,34 @@ import { getUser } from "./controllers/getUserController";
 import dotenv from "dotenv";
 dotenv.config();
 const server = http.createServer((req, res) => {
-
-     if (req.method === "GET" && req.url!.match(/\/api\/users\/\w+/)) {
-        const id = req.url!.split("/")[3];
-        return getUser(res, id);
-    } 
-    if (req.method == "GET" && (req.url == "/api/users" || req.url == "/api/users/")) {
-        getUsers(res);
+    try{
+        if (req.method === "GET" && req.url!.match(/\/api\/users\/\w+/)) {
+            const id = req.url!.split("/")[3];
+            return getUser(res, id);
+        }
+        if (req.method == "GET" && (req.url == "/api/users" || req.url == "/api/users/")) {
+            getUsers(res);
+        }
+    
+        if (req.method == "POST" && (req.url == "/api/users" || req.url == "/api/users/")) {
+            return addUser(req, res);
+        }
+    
+        if (req.method === "PUT" && req.url!.match(/\/api\/users\/\w+/)) {
+            const id = req.url!.split("/")[3];
+            return updateUser(req, res, id);
+        }
+    
+        if (req.method == "DELETE" && req.url!.match(/\/api\/users\/\w+/)) {
+            const id = req.url!.split("/")[3];
+            return deleteUser(req, res, id);
+        }
+    } catch(err) {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: 'Resource doesn\'t exist!' }));
     }
 
-    if (req.method == "POST" && (req.url == "/api/users" || req.url == "/api/users/")) {
-        return addUser(req, res);
-    }
-
-    if (req.method === "PUT" && req.url!.match(/\/api\/users\/\w+/)) {
-        const id = req.url!.split("/")[3];
-        return updateUser(req, res, id);    
-    }
-
-    if (req.method == "DELETE" && req.url!.match(/\/api\/users\/\w+/)) {
-        const id = req.url!.split("/")[3];
-        return deleteUser(req, res, id);
-    }
+    
 });
 
 server.listen(process.env.PORT, () => {
