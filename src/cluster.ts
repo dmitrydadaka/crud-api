4efc45d2b73dd9022e4c714403e3f7a6bc194d7a
User <belpc2012@gmail.com>
2022-06-24 18:42:25 +0300

    feat:add cluster

--- src/cluster.ts
+++ src/cluster.ts
@@ -1,22 +1,23 @@
-import * as Cluster from 'cluster';
+import cluster from 'cluster';
 import { cpus } from 'os';
 import { server } from './index';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const numCPUs = cpus().length;
-const cluster = Cluster as unknown as Cluster.Cluster;
 
 if (cluster.isPrimary) {
+  
   console.log(`Primary ${process.pid} is running`);
 
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
-
   cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
+    console.log('worker %d died (%s).',
+                worker.process.pid, signal || code);
   });
+
 } else {
   const PORT = process.env.PORT;
   server.listen(PORT, () => {
@@ -24,3 +25,5 @@ if (cluster.isPrimary) {
   });
   console.log(`Worker ${process.pid} started`);
 }
+
+export { cluster };
